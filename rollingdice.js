260e644e2b04129b2be05bd81771d3df02871acb
Tracy DeWitt <Justdewitt8485@gmail.com>
2020-03-05 16:56:30 -0600

    Working on the Logic for the JS

---
+++ rollingdice.js
@@ -0,0 +1,15 @@
+let NumCount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let RandNum = Math.ceil(Math.random() * 6) + Math.ceil(Math.random() * 6);
+
+for (let i = 0; i < 1000; i++) 
+{
+    NumCount[RandNum] = NumCount[RandNum] + 1;
+
+
+    
+}
+
+
+
+
+
